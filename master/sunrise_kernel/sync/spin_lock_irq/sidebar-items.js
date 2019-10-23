0581dcdbb4008bea6e5f0e8070571035b0efbe11
roblabla <github@roblab.la>
2019-10-23 13:26:28 +0000

    Deploying to gh-pages from master 12e69ba1dc1935bea71d4b80ddc6bcb3303400ea

---
+++ master/sunrise_kernel/sync/spin_lock_irq/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["permanently_disable_interrupts","Permanently disables the interrupts. Forever."]],"static":[["INTERRUPT_DISARM","Boolean to [permanently_disable_interrupts]."]],"struct":[["SpinLockIRQ","SpinLock that disables IRQ."],["SpinLockIRQGuard","The SpinLockIrq lock guard."]]});
